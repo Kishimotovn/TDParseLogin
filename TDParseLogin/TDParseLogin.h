2c94cbd2beef676f753e74751b1337c1fe7560df
Anh Phan Tran <anh@thedistance.co.uk>
2017-02-14 21:28:47 +0000

    Initial Commit

---
+++ TDParseLogin/TDParseLogin.h
@@ -0,0 +1,19 @@
+//
+//  TDParseLogin.h
+//  TDParseLogin
+//
+//  Created by Anh Phan Tran on 2/14/17.
+//  Copyright © 2017 The Distance. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TDParseLogin.
+FOUNDATION_EXPORT double TDParseLoginVersionNumber;
+
+//! Project version string for TDParseLogin.
+FOUNDATION_EXPORT const unsigned char TDParseLoginVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TDParseLogin/PublicHeader.h>
+
+
